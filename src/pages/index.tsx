088bc4e299d8882eeb0b03dae6c045e894b48c24
unknown <nguyencongcuong9809@gmail.com>
2025-03-17 21:59:20 +0700

    Updated code111

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,6 @@ import { useEffect } from 'react'
 export default function Home() {
 
   const fetchApiUser = async () => {
-<<<<<<< HEAD
-    await axios.get("http://localhost:3001/api/users?limit=10&page=1&order=created%20asc").then((res)=> {
-      console.log("res")
-    })
-  }
-  useEffect(()=>{
-    fetchApiUser
-  },[])
-
-=======
     await axios.get("http://localhost:3001/api/users?limit=10&page=1&order=created%20asc").then((res) =>{
       console.log("res")
     })
@@ -24,7 +14,6 @@ export default function Home() {
     useEffect(() => {
     fetchApiUser()
   },[])
->>>>>>> 1dca977db40fe6602c07cdc68dbae4157dbee338
   return (
     <>
       <Head>
@@ -33,11 +22,7 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-<<<<<<< HEAD
-      <Button variant='contained'>Hello world Updated1111</Button>
-=======
       <Button variant='contained'>Hello world Updated</Button>
->>>>>>> 1dca977db40fe6602c07cdc68dbae4157dbee338
     </>
   )
 }
